1e33bf0bc4e2df6d58c573045c9a4b25e427c012
nguyendangkhoa-flutter <149855121+nguyendangkhoa-flutter@users.noreply.github.com>
2024-07-18 11:09:10 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clusterkhoa.75jpstb.mongodb.net',
+    DB_USER: 'khoa6103',
+    DB_PASS: 'khoadang6103',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'sova',
+    JWT_EXPIRES: '360000000', // in milliseconds
+    EMAIL_USER: 'nguyendangkhoa6103@gmail.com', // gmail service
+    EMAIL_PASS: 'reyd ujwq xzae ylds'
+  };
+  module.exports = MyConstants;
