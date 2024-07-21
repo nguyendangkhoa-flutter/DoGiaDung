3b638ea4e2f268d611004e89f24b98e821f649a1
nguyendangkhoa-flutter <149855121+nguyendangkhoa-flutter@users.noreply.github.com>
2024-07-22 00:17:55 +0700

    Update StatisticsComponent.js

--- client-admin/src/components/StatisticsComponent.js
+++ client-admin/src/components/StatisticsComponent.js
@@ -7,21 +7,21 @@ class Statistics extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            noCategories = 0,
-            noProducts = 0,
-            noOrders = 0,
-            noOrdersPending = 0,
-            noOrdersApproved = 0,
-            noOrdersCanceled = 0,
-            noOrdersRevenue = 0,
-            noCustomers = 0
+            noCategories : 0,
+            noProducts : 0,
+            noOrders : 0,
+            noOrdersPending : 0,
+            noOrdersApproved : 0,
+            noOrdersCanceled : 0,
+            noOrdersRevenue : 0,
+            noCustomers : 0
         };
     }
     render() {
         return  (
             <div className="text-center">
                 <h2 className='text-center'>STATISTICS</h2>
-                <table className='align-center'></table>
+                <table className='align-center'>
                     <tbody>
                         <tr>
                             <td align="right">No.Categories</td>
@@ -29,50 +29,51 @@ class Statistics extends Component {
                             <td>{this.state.noCategories}</td>
                         </tr>
                         <tr>
-                        <td align="right">No.Products</td>
+                            <td align="right">No.Products</td>
                             <td></td>
                             <td>{this.state.noProducts}</td>    
                         </tr>
                         <tr>
-                        <td align="right">No.Orders</td>
+                            <td align="right">No.Orders</td>
                             <td></td>
                             <td>{this.state.noOrders}</td> 
                         </tr>
                         <tr>
-                        <td align="right">Pending &ensp;</td>
                             <td></td>
+                            <td align="right">Pending &ensp;</td>                           
                             <td>{this.state.noOrdersPending}</td> 
                         </tr>
                         <tr>
-                        <td align="right">Approved &ensp;</td>
                             <td></td>
+                            <td align="right">Approved &ensp;</td>                          
                             <td>{this.state.noOrdersApproved}</td> 
                         </tr>
                         <tr>
-                        <td align="right">Canceled &ensp;</td>
                             <td></td>
+                            <td align="right">Canceled &ensp;</td>
                             <td>{this.state.noOrdersCanceled}</td> 
                         </tr>
                         <tr>
-                        <td align="right">Revenue &ensp;</td>
                             <td></td>
+                            <td align="right">Revenue &ensp;</td>                         
                             <td>{this.state.noOrdersRevenue}</td> 
                         </tr>
-                        <tr>
-                        <td align="right">No.Customers</td>
-                            <td></td>
+                        <tr>                            
+                            <td align="right">No.Customers</td> 
+                            <td></td>                        
                             <td>{this.state.noCustomers}</td> 
                         </tr>
                     </tbody>
+                </table>
             </div>
         );
     }
     componentDidMount() {
-        this.appGetStatistics();
+        this.apiGetStatistics();
     }
-    // api
+    // apis
     apiGetStatistics() {
-        const config = { haders: {'x-access-token':this.context.token} };
+        const config = { headers: {'x-access-token':this.context.token} };
         axios.get('/api/admin/statistics', config).then((res) => {
             const result = res.data;
             this.setState({
